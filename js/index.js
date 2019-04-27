fc8fdd6e90eb17ce78d0079d7f35afba4d4e84a9
kylewetzler <45954950+kylewetzler@users.noreply.github.com>
2019-04-27 16:32:44 -0400

    fixes

--- js/index.js
+++ js/index.js
@@ -10,34 +10,7 @@ app.controller('mainCtrl', function($scope) {
 	},{
 		name: 'Explore',
 		image: 'https://source.unsplash.com/7BjmDICVloE/900x900'
-	}, {
-		name: 'Vast',
-		image: 'https://source.unsplash.com/WLUHO9A_xik/900x900'
-	}, {
-		name: 'Playful',
-		image: 'https://source.unsplash.com/b2-fBVrfx0o/900x900'
-	}, {
-		name: 'Grand',
-		image: 'https://source.unsplash.com/Ixp4YhCKZkI/900x900'
-	}, {
-		name: 'Mist',
-		image: 'https://source.unsplash.com/8BmNurlVR6M/900x900'
-	}, {
-		name: 'Sea',
-		image: 'https://source.unsplash.com/6YqpFWWQsno/900x900'
-	}, {
-		name: 'Reach',
-		image: 'https://source.unsplash.com/zFnk_bTLApo/900x900'
-	}, {
-		name: 'Awe',
-		image: 'https://source.unsplash.com/j4PaE7E2_Ws/900x900'
-	}, {
-		name: 'Surf',
-		image: 'https://source.unsplash.com/uohGiEVhWiQ/900x900'
-	}, {
-		name: 'Thrill',
-		image: 'https://source.unsplash.com/ssrbaKvxaos/900x900'
-	}, ];
+	}];
 
 	$scope.selected = [];
 	$scope.selectBox = function(item, position) {
